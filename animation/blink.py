f639c964d86f73422e3e2cdb709938a2c7c11e89
mrgrose <grachev.ro@gmail.com>
2025-08-13 15:42:49 +0300

    Adding offset_tics

--- animation/blink.py
+++ animation/blink.py
@@ -1,26 +1,25 @@
 import asyncio
 import curses
-import random
 
 
-async def blink(canvas, row, column, symbol="*"):
+async def blink(canvas, row, column, symbol, offset_tics):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
         canvas.refresh()
-        for _ in range(random.randint(0, 20)):
+        for _ in range(offset_tics):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
         canvas.refresh()
-        for _ in range(random.randint(0, 3)):
+        for _ in range(offset_tics):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
         canvas.refresh()
-        for _ in range(random.randint(0, 5)):
+        for _ in range(offset_tics):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
         canvas.refresh()
-        for _ in range(random.randint(0, 3)):
+        for _ in range(offset_tics):
             await asyncio.sleep(0)
