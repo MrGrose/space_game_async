d7a637881516b1cb4dcfeaa53665c0d0f6fa0e43
mrgrose <grachev.ro@gmail.com>
2025-08-15 13:46:10 +0300

    Del canvas.refresh()

--- animation/blink.py
+++ animation/blink.py
@@ -5,21 +5,17 @@ import curses
 async def blink(canvas, row, column, symbol, offset_tics):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        canvas.refresh()
         for _ in range(offset_tics):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        canvas.refresh()
         for _ in range(offset_tics):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        canvas.refresh()
         for _ in range(offset_tics):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        canvas.refresh()
         for _ in range(offset_tics):
             await asyncio.sleep(0)
